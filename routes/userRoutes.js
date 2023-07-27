a39a64ee19efba9b0d880d409f2193a1bd08ba2d
Ramprakash <ramprakashramtekkar@gmail.com>
2023-07-27 22:37:24 +0530

    done

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { loginController, registerController } = require("../controller/userController");
+
+//router object
+const router = express.Router()
+
+//routes
+//POST || LOGIN
+router.post("/login", loginController);
+
+//POST || REGISTER
+router.post("/register", registerController);
+
+
+module.exports = router;
